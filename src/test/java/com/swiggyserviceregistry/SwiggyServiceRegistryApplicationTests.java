281415c57ec5cc360e9c23129db23b8cc2915f15
Satya@488 <sahoosatyabrat844@gmail.com>
2023-06-19 00:06:20 +0530

    SwiggyServiceRegistry Backup

---
+++ src/test/java/com/swiggyserviceregistry/SwiggyServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swiggyserviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwiggyServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
